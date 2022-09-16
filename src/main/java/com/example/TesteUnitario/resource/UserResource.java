5ce526f907f95bd6553858047ab4ae281fc19011
Danrleydev <danrleysouza53@gmail.com>
2022-09-16 09:57:44 -0300

    ultilizando o desing pattern dto

--- src/main/java/com/example/TesteUnitario/resource/UserResource.java
+++ src/main/java/com/example/TesteUnitario/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.example.TesteUnitario.resource;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.TesteUnitario.domain.User;
+import com.example.TesteUnitario.domain.dto.UserDTO;
 import com.example.TesteUnitario.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 	
+	@Autowired ModelMapper mapper;
+	
 	@Autowired
 	private UserService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(service.findById(id));
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 	}
 }
